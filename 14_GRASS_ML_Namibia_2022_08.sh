e6496e81daf59365777d1af371050a8039ba3023
Polina Lemenkova <pauline.lemenkova@gmail.com>
2023-12-26 17:10:05 +0100

    Add files via upload

--- 14_GRASS_ML_Namibia_2022_08.sh
+++ 14_GRASS_ML_Namibia_2022_08.sh
@@ -19,15 +19,22 @@ g.list rast
 #
 # ----CREATING COLOR COMPOSITES-------------------------->
 # false color
-r.composite blue=L8_2022_G_07 green=L8_2022_G_05 red=L8_2022_G_03 output=L8_2022_G_753 --overwrite
+r.composite blue=L8_2022_G_07 green=L8_2022_G_04 red=L8_2022_G_01 output=L8_2022_G_741 --overwrite
+g.region raster=L8_2022_G_07 -o
 d.mon wx0
-d.rast L8_2022_G_753
-d.out.file output=Namibia_753 format=jpg --overwrite
+d.rast L8_2022_G_741
+d.out.file output=Namibia_J_741 format=jpg --overwrite
 # false color: NIR band B05 in the red channel, red band B04 in the green channel and green band B03 in the blue channel
 r.composite blue=L8_2022_G_03 green=L8_2022_G_04 red=L8_2022_G_05 output=L8_2022_G_345 --overwrite
+g.region raster=L8_2022_G_03 -o
 d.mon wx0
 d.rast L8_2022_G_345
-d.out.file output=Namibia_345 format=jpg --overwrite
+d.out.file output=Namibia_A_345 format=jpg --overwrite
+# true color
+r.composite blue=L8_2022_G_02 green=L8_2022_G_03 red=L8_2022_G_04 output=L8_2022_G_234 --overwrite
+d.mon wx0
+d.rast L8_2022_G_234
+d.out.file output=Namibia_A_234 format=jpg --overwrite
 
 # ---CLUSTERING AND CLASSIFICATION------------------->
 # grouping data by i.group
